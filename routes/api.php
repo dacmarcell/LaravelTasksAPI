4e147510f7eed07584733654385d850eeb26b32f
dacmarcell <dacmarcell@gmail.com>
2025-01-08 01:08:17 -0300

    feat: add routes for CategoryController and task-category relationship

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('tasks', [TaskController::class, 'index']);
 Route::post('tasks', [TaskController::class, 'store']);
+Route::post("tasks/{id}/categories", [TaskController::class, "addCategory"]);
 Route::get('tasks/{id}', [TaskController::class, 'show']);
 Route::put('tasks/{id}', [TaskController::class, 'update']);
 Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
+
+Route::get("categories", [CategoryController::class, "index"]);
+Route::post("categories", [CategoryController::class, "store"]);
+Route::get("categories/{id}", [CategoryController::class, "show"]);
+Route::put("categories/{id}", [CategoryController::class, "update"]);
+Route::delete("categories/{id}", [CategoryController::class, "destroy"]);
